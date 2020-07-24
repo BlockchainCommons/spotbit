16417923e9e4bbb6f6660ef8ef4a25e8bc6860b8
christian <christian.b.murray.21@dartmouth.edu>
2020-07-24 10:38:37 -0400

    removed btctradeim from list of exchanges temporarily because it is found within ccxt, but only on my raspberry pi. Also fixed the arguments to the prune thread and added more line comments...

--- server.py
+++ server.py
@@ -31,8 +31,9 @@ app = Flask(__name__)
 # Future Plans:
 # Hard coding supported exchanges is a bad practice. CCXT autogenerates code for each exchange and therefore at least in theory may frequently support new exchanges.
 # Need to find a way to automatically create a list of exchange objects. 
+# btctradeim doesn't want to work on raspberry pi
 def init_supported_exchanges():
-    objects = {"acx":ccxt.acx(), "anxpro":ccxt.anxpro(), "aofex":ccxt.aofex(), "bcex":ccxt.bcex(), "bequant":ccxt.bequant(), "bibox":ccxt.bibox(), "bigone":ccxt.bigone(), "binance":ccxt.binance(), "bit2c":ccxt.bit2c(), "bitbank":ccxt.bitbank(), "bitbay":ccxt.bitbay(), "bitfinex":ccxt.bitfinex(), "bitflyer":ccxt.bitflyer(), "bitforex":ccxt.bitforex(), "bithumb":ccxt.bithumb(), "bitkk":ccxt.bitkk(), "bitmart":ccxt.bitmart(), "bitmax":ccxt.bitmax(), "bitstamp":ccxt.bitstamp(), "bittrex":ccxt.bittrex(), "bitz":ccxt.bitz(), "bl3p":ccxt.bl3p(), "bleutrade":ccxt.bleutrade(), "braziliex":ccxt.braziliex(), "btcalpha":ccxt.btcalpha(), "btcbox":ccxt.btcbox(), "btcmarkets":ccxt.btcmarkets(), "btctradeim":ccxt.btctradeim(), "btctradeua":ccxt.btctradeua(), "btcturk":ccxt.btcturk(), "buda":ccxt.buda(), "bw":ccxt.bw(), "bybit":ccxt.bybit(), "bytetrade":ccxt.bytetrade(), "cex":ccxt.cex(), "chilebit":ccxt.chilebit(), "coinbase":ccxt.coinbase(), "coincheck":ccxt.coincheck(), "coinegg":ccxt.coinegg(), "coinex":ccxt.coinex(), "coinfalcon":ccxt.coinfalcon(), "coinfloor":ccxt.coinfloor(), "coingi":ccxt.coingi(), "coinmarketcap":ccxt.coinmarketcap(), "coinmate":ccxt.coinmate(), "coinone":ccxt.coinone(), "coinspot":ccxt.coinspot(), "coolcoin":ccxt.coolcoin(), "coss":ccxt.coss(), "crex24":ccxt.crex24(), "currencycom":ccxt.currencycom(), "deribit":ccxt.deribit(), "digifinex":ccxt.digifinex(), "dsx":ccxt.dsx(), "eterbase":ccxt.eterbase(), "exmo":ccxt.exmo(), "exx":ccxt.exx(), "fcoin":ccxt.fcoin(), "fcoinjp":ccxt.fcoinjp, "flowbtc":ccxt.flowbtc(), "foxbit":ccxt.foxbit(), "ftx":ccxt.ftx(), "fybse":ccxt.fybse(), "gateio":ccxt.gateio(), "gemini":ccxt.gemini(), "hbtc":ccxt.hbtc(), "hitbtc":ccxt.hitbtc(), "hollaex":ccxt.hollaex(), "huobipro":ccxt.huobipro(), "ice3x":ccxt.ice3x(), "idex":ccxt.idex(), "independentreserve":ccxt.independentreserve(), "indodax":ccxt.indodax(), "itbit":ccxt.itbit(), "kraken":ccxt.kraken(), "kucoin":ccxt.kucoin(), "kuna":ccxt.kuna(), "lakebtc":ccxt.lakebtc(), "latoken":ccxt.latoken(), "lbank":ccxt.lbank(), "liquid":ccxt.liquid(), "livecoin":ccxt.livecoin(), "luno":ccxt.luno(), "lykke":ccxt.lykke(), "mercado":ccxt.mercado(), "mixcoins":ccxt.mixcoins(), "oceanex":ccxt.oceanex(), "okcoin":ccxt.okcoin(), "okex":ccxt.okex(), "paymium":ccxt.paymium(), "poloniex":ccxt.poloniex(), "probit":ccxt.probit(), "qtrade":ccxt.qtrade(), "rightbtc":ccxt.rightbtc(), "southxchange":ccxt.southxchange(), "stex":ccxt.stex(), "stronghold":ccxt.stronghold(), "surbitcoin":ccxt.surbitcoin(), "therock":ccxt.therock(), "tidebit":ccxt.tidebit(), "tidex":ccxt.tidex(), "upbit":ccxt.upbit(), "vaultoro":ccxt.vaultoro(), "vbtc":ccxt.vbtc(), "wavesexchange":ccxt.wavesexchange(), "whitebit":ccxt.whitebit(), "xbtce":ccxt.xbtce(), "yobit":ccxt.yobit(), "zaif":ccxt.zaif(), "zb":ccxt.zb()}
+    objects = {"acx":ccxt.acx(), "anxpro":ccxt.anxpro(), "aofex":ccxt.aofex(), "bcex":ccxt.bcex(), "bequant":ccxt.bequant(), "bibox":ccxt.bibox(), "bigone":ccxt.bigone(), "binance":ccxt.binance(), "bit2c":ccxt.bit2c(), "bitbank":ccxt.bitbank(), "bitbay":ccxt.bitbay(), "bitfinex":ccxt.bitfinex(), "bitflyer":ccxt.bitflyer(), "bitforex":ccxt.bitforex(), "bithumb":ccxt.bithumb(), "bitkk":ccxt.bitkk(), "bitmart":ccxt.bitmart(), "bitmax":ccxt.bitmax(), "bitstamp":ccxt.bitstamp(), "bittrex":ccxt.bittrex(), "bitz":ccxt.bitz(), "bl3p":ccxt.bl3p(), "bleutrade":ccxt.bleutrade(), "braziliex":ccxt.braziliex(), "btcalpha":ccxt.btcalpha(), "btcbox":ccxt.btcbox(), "btcmarkets":ccxt.btcmarkets(), "btctradeua":ccxt.btctradeua(), "btcturk":ccxt.btcturk(), "buda":ccxt.buda(), "bw":ccxt.bw(), "bybit":ccxt.bybit(), "bytetrade":ccxt.bytetrade(), "cex":ccxt.cex(), "chilebit":ccxt.chilebit(), "coinbase":ccxt.coinbase(), "coincheck":ccxt.coincheck(), "coinegg":ccxt.coinegg(), "coinex":ccxt.coinex(), "coinfalcon":ccxt.coinfalcon(), "coinfloor":ccxt.coinfloor(), "coingi":ccxt.coingi(), "coinmarketcap":ccxt.coinmarketcap(), "coinmate":ccxt.coinmate(), "coinone":ccxt.coinone(), "coinspot":ccxt.coinspot(), "coolcoin":ccxt.coolcoin(), "coss":ccxt.coss(), "crex24":ccxt.crex24(), "currencycom":ccxt.currencycom(), "deribit":ccxt.deribit(), "digifinex":ccxt.digifinex(), "dsx":ccxt.dsx(), "eterbase":ccxt.eterbase(), "exmo":ccxt.exmo(), "exx":ccxt.exx(), "fcoin":ccxt.fcoin(), "fcoinjp":ccxt.fcoinjp, "flowbtc":ccxt.flowbtc(), "foxbit":ccxt.foxbit(), "ftx":ccxt.ftx(), "fybse":ccxt.fybse(), "gateio":ccxt.gateio(), "gemini":ccxt.gemini(), "hbtc":ccxt.hbtc(), "hitbtc":ccxt.hitbtc(), "hollaex":ccxt.hollaex(), "huobipro":ccxt.huobipro(), "ice3x":ccxt.ice3x(), "idex":ccxt.idex(), "independentreserve":ccxt.independentreserve(), "indodax":ccxt.indodax(), "itbit":ccxt.itbit(), "kraken":ccxt.kraken(), "kucoin":ccxt.kucoin(), "kuna":ccxt.kuna(), "lakebtc":ccxt.lakebtc(), "latoken":ccxt.latoken(), "lbank":ccxt.lbank(), "liquid":ccxt.liquid(), "livecoin":ccxt.livecoin(), "luno":ccxt.luno(), "lykke":ccxt.lykke(), "mercado":ccxt.mercado(), "mixcoins":ccxt.mixcoins(), "oceanex":ccxt.oceanex(), "okcoin":ccxt.okcoin(), "okex":ccxt.okex(), "paymium":ccxt.paymium(), "poloniex":ccxt.poloniex(), "probit":ccxt.probit(), "qtrade":ccxt.qtrade(), "rightbtc":ccxt.rightbtc(), "southxchange":ccxt.southxchange(), "stex":ccxt.stex(), "stronghold":ccxt.stronghold(), "surbitcoin":ccxt.surbitcoin(), "therock":ccxt.therock(), "tidebit":ccxt.tidebit(), "tidex":ccxt.tidex(), "upbit":ccxt.upbit(), "vaultoro":ccxt.vaultoro(), "vbtc":ccxt.vbtc(), "wavesexchange":ccxt.wavesexchange(), "whitebit":ccxt.whitebit(), "xbtce":ccxt.xbtce(), "yobit":ccxt.yobit(), "zaif":ccxt.zaif(), "zb":ccxt.zb()}
     return objects
 
 # Check if a given exchange is in the list of supported exchanges.
@@ -84,6 +85,7 @@ def configure():
 def now(currency, exchange):
     db_n = sqlite3.connect(p)
     if exchange in exchanges:
+        #if the exchange is already in the config file
         ticker = "BTC-{}".format(currency.upper())
         statement = "SELECT * FROM {} WHERE pair = '{}' AND timestamp = (SELECT MAX(timestamp) FROM {}) LIMIT 1;".format(exchange, ticker, exchange)
         cursor = db_n.execute(statement)
@@ -103,10 +105,12 @@ def now(currency, exchange):
 @app.route('/hist/<currency>/<exchange>/<date_start>/<date_end>', methods=['GET'])
 def hist(currency, exchange, date_start, date_end):
     db_n = sqlite3.connect(p)
+    #check what format of dates we have
     if (str(date_start)).isdigit():
         date_s = int(date_start)         
         date_e = int(date_end) 
     else:
+        #error checking for malformed dates
         try:
             date_s = (datetime.fromisoformat(date_start.replace("T", " "))).timestamp()*1000
             date_e = (datetime.fromisoformat(date_end.replace("T", " "))).timestamp()*1000
@@ -257,20 +261,22 @@ def install():
 # Remove every entry older than now-keepWeeks from all tables in the database
 # if there is nothing to prune then nothing will be pruned.
 def prune(keepWeeks):
+    # prune checks will run continuously and check every 60k seconds right now.
+    db_n = sqlite3.connect(p)
     while True:
         for exchange in exchanges:
             #count = ((db.execute("SELECT Count(*) FROM {}".format(exchange))).fetchone())[0]
             cutoff = (datetime.now()-timedelta(weeks=keepWeeks)).timestamp()*1000
             statement = "DELETE FROM {} WHERE timestamp < {};".format(exchange, cutoff)
-            db.execute(statement)
-            db.commit()
+            db_n.execute(statement)
+            db_n.commit()
         time.sleep(60000)
     
 
 if __name__ == "__main__":
     install() #install will call read_config
     prices_thread = Thread(target=request_periodically, args=(exchanges, currency, interval))
-    pruning_thread = Thread(target=prune, args=(keepWeeks))
+    pruning_thread = Thread(target=prune, args=[keepWeeks])
     prices_thread.start()
     pruning_thread.start()
     app.run()
