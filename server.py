fd349c37e6a0d755971c63010c2e44bdc3438795
bwakedavis <bwakedavis@gmail.com>
2022-08-01 07:31:42 -0700

    Move the code and assets in www directory to the templates and static directories. Rewrite server.py: @app.get('/') and @app.get('/faq') to serve the front-end.

--- server.py
+++ server.py
@@ -252,13 +252,22 @@ def request_single(exchange: ccxt.Exchange, currency: CurrencyName) -> Candle | 
 # TODO(nochiel) Put the api behind an /api/v1 path.
 
 # TODO(nochiel) Make this the Spotbit frontend.
-@app.get('/')
-def index():
-
-    raise HTTPException(
-            detail  =  'Not implemented.',
-            status_code = 404
-            )
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+@app.get('/', response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+    # raise HTTPException(
+    #         detail  =  'Not implemented.',
+    #         status_code = 404
+    #         )
+@app.get("/faq", response_class=HTMLResponse)
+async def main(request: Request):
+    return templates.TemplateResponse("faq.html", {"request": request})
 
 
 @app.get('/api/status')
